3ed053327a284b726f2881f38c4a730c1963ec17
Margaux Sleckman <msleckman@usgs.gov>
2021-11-26 11:16:02 -0800

    updates to get_nwis_data() --> additional funtion for saving dnloaded csv if desired

--- 1_fetch/src/get_nwis_data.R
+++ 1_fetch/src/get_nwis_data.R
@@ -1,7 +1,7 @@
-download_nwis_data <- function(site_num, parameterCd = '00010', startDate = "2014-05-01", endDate = "2015-05-01"){
-  
+download_nwis_data <- function(site_num, parameterCd, startDate, endDate){
+
   data_out <- readNWISdata(sites = site_num, parameterCd = parameterCd, startDate = startDate, endDate = endDate, service="iv")
-  
+
   # -- simulating a failure-prone web-service here, do not edit --
   set.seed(Sys.time())
   if (sample(c(T,F,F,F), 1)){
@@ -12,26 +12,29 @@ download_nwis_data <- function(site_num, parameterCd = '00010', startDate = "201
   
 }
 
-## Combine nwis datasets and save the full dataset if desired  
-combine_nwis_data <- function(..., Save_as_csv = F){
+## Combine nwis datasets 
+combine_nwis_data <- function(...){
   
   site_data <- bind_rows(...)
   
-  ## Added in case user wants the save the data extracted from NWIS to local dir, Default is False for now
-  if(Save_as_csv == TRUE){
-    dir <- getwd()
-    file_path <- file.path(dir,"1_fetch/out","nwis_data.csv")
-    print(file_path)
-    write_csv(data_out, file_path)
-    }
   return(site_data)
   
+}
+
+## Save the full downloaded dataset if desired  
+save_nwis_data_csv <- function(Save_as_csv = F, data, fileout = "1_fetch/out/nwis_data.csv"){
+
+  # Added in case user wants the save the data extracted from NWIS to local dir, Default is False for now
+  if(Save_as_csv == TRUE){
+    write_csv(data, fileout)
   }
 
+  return(fileout)
+}
 
 ## Export selected site info from nwis
-nwis_site_info <- function(fileout, site_data){
-  site_no <- unique(site_data$site_no)
+nwis_site_info <- function(fileout, data){
+  site_no <- unique(data$site_no)
   site_info <- dataRetrieval::readNWISsite(site_no)
   write_csv(site_info, fileout)
   return(fileout)
--- _targets.R
+++ _targets.R
@@ -6,44 +6,76 @@ source("3_visualize/src/plot_timeseries.R")
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("tidyverse", "dataRetrieval")) # Loading tidyverse because we need dplyr, ggplot2, readr, stringr, and purrr
 
+## Define vars
+site_1 <- "01427207"
+site_2 <- '01432160'
+site_3 <- "01436690"
+site_4 <- '01466500'
+parameterCd <- '00010'
+startDate <- "2014-05-01"
+endDate <- "2015-05-01"
+fetch_out_folder <- "1_fetch/out"
+
+## Fetch and save downloaded nwis data info
 p1_targets_list <- list(
   tar_target(
     site_data_01427207,
-    download_nwis_data(site_num = "01427207", parameterCd = '00010', startDate = "2014-05-01", endDate = "2015-05-01"),
-  ),
+    download_nwis_data(site_num = site_1,
+                       parameterCd = parameterCd,
+                       startDate = startDate,
+                       endDate = endDate),
+    ),
   
   tar_target(
     site_data_01432160,
-    download_nwis_data(site_num = "01432160", parameterCd = '00010', startDate = "2014-05-01", endDate = "2015-05-01"),
+    download_nwis_data(site_num = site_2,
+                       parameterCd = parameterCd,
+                       startDate = startDate,
+                       endDate = endDate),
   ),
-  
+
   tar_target(
     site_data_01436690,
-    download_nwis_data(site_num = "01436690", parameterCd = '00010', startDate = "2014-05-01", endDate = "2015-05-01"),
+    download_nwis_data(site_num = site_3,
+                       parameterCd = parameterCd,
+                       startDate = startDate,
+                       endDate = endDate),
   ),
-  
+
   tar_target(
     site_data_01466500,
-    download_nwis_data(site_num =  "01466500", parameterCd = '00010', startDate = "2014-05-01", endDate = "2015-05-01"),
+    download_nwis_data(site_num =  site_4,
+                       parameterCd = parameterCd,
+                       startDate = startDate,
+                       endDate = endDate),
   ),
-  
+
   tar_target(
     site_data,
     combine_nwis_data(site_data_01427207,
                       site_data_01432160,
-                      site_data_01436690, 
-                      site_data_01466500,
-                      Save_as_csv = F),
+                      site_data_01436690,
+                      site_data_01466500),
   ),
-  
   
   tar_target(
     site_info_csv,
-    nwis_site_info(fileout = "1_fetch/out/site_info.csv", site_data),
+    nwis_site_info(fileout = file.path(fetch_out_folder, 'site_info.csv'),
+                   data = site_data),
+    format = "file"
+  ),
+  
+  tar_target(
+    site_data_csv,
+    save_nwis_data_csv(Save_as_csv = F,
+                       fileout = file.path(fetch_out_folder,"nwis_data.csv"),
+                       data = site_data),
     format = "file"
   )
+
 )
 
+## Process
 p2_targets_list <- list(
   tar_target(
     site_data_clean, 
@@ -59,6 +91,7 @@ p2_targets_list <- list(
   )
 )
 
+## Visualize
 p3_targets_list <- list(
   tar_target(
     figure_1_png,
@@ -67,5 +100,5 @@ p3_targets_list <- list(
   )
 )
 
-# Return the complete list of targets
+## Return the complete list of targets
 c(p1_targets_list, p2_targets_list, p3_targets_list)
